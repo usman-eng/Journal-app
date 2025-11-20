e6718d85415f993caef6670e4210c1d1d15f713f
usman-eng <usmansoftsolu@gmail.com>
2025-11-20 10:29:19 +0300

    use eleven lab api and add values in yml file

--- src/main/java/com/example/demo/Service/WeatherService.java
+++ src/main/java/com/example/demo/Service/WeatherService.java
@@ -2,16 +2,21 @@ package com.example.demo.Service;
 
 import com.example.demo.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
 
-    private static final String Apikey="4dcce3ec83267757ebd020e2330918b11";
+    @Value("${weather_api_key}")
+    private String Apikey;
     private static final String URL="http://api.weatherstack.com/current?access_key=Apikey&query=CITY";
+    @Value("${elevenLab_api_key}")
+    private String ElevenLabKey;
     @Autowired
     private RestTemplate restTemplate;
 
